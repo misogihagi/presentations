20503b1578a61b17477284ec079560e5938d2064
misogihagi <47350059+misogihagi@users.noreply.github.com>
2025-05-21 23:03:12 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ entries=`find * -maxdepth 0 -type d`
 for directory in $entries; do
     cd $directory
     pnpm install
-    npx slidev build --base /presentaions/$directory
+    pnpm build --base /presentaions/$directory
     cd ..
 done
