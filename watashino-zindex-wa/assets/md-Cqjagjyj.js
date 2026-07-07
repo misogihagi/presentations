c3a88719cb9c4a0a2048622e195c19ded730b68d
misogihagi <misogihagi@users.noreply.github.com>
2026-07-07 15:17:26 +0000

    pages from 8fc59da0d172ae8c9a81e405d137e48cc04375b8

---
+++ watashino-zindex-wa/assets/md-Cqjagjyj.js
@@ -0,0 +1,50 @@
+import{_ as e}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-CSbPb4En.js";import{b as p,o as r,w as l,g as s,e as d,m as g,ad as i,v as o,x as y,T as n}from"./modules/vue-CBHG3zdT.js";import{I as c}from"./slidev/default-C_JkXc79.js";import{u as D,f as B}from"./slidev/context-CJwE3xOY.js";import"./modules/unplugin-icons-C2Hc2Qst.js";import"./index-OpOTB6_k.js";import"./modules/shiki-Bxaic-fK.js";const z={__name:"slides.md__slidev_5",setup(A){const{$clicksContext:h,$frontmatter:k}=D();return h.setup(),(C,a)=>{const t=e;return r(),p(c,o(y(n(B)(n(k),4))),{default:l(()=>[a[1]||(a[1]=s("h4",null,"例",-1)),a[2]||(a[2]=s("p",null,[s("a",{href:"https://codepen.io/misogihagi/pen/gbrxOzM",target:"_blank"},"https://codepen.io/misogihagi/pen/gbrxOzM")],-1)),a[3]||(a[3]=s("ol",null,[s("li",null,[s("p",null,"赤い背景の子要素 (z-index: 999) は、青い枠の親要素 (overflow: hidden) の右側と下側で切り取られて（クリッピングされて）表示されます。")]),s("li",null,[s("p",null,"親要素の兄弟である緑色の要素 (z-index: 100) は、子要素よりも z-index が低いにもかかわらず、切り取られた赤い子要素の上に表示されます。")])],-1)),d(t,g({},{title:"",ranges:[]}),{default:l(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-html"},[s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"<"),s("span",{style:{"--shiki-dark":"#4D9375","--shiki-light":"#1E754F"}},"div"),s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}}," style"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"="),s("span",{style:{"--shiki-dark":"#C98A7D77","--shiki-light":"#B5695977"}},'"')]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        position: relative;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        top: 0px; /* 親要素と位置を少し重ねるため */")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        width: 250px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        height: 80px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        margin: 50px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        padding: 10px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        z-index: 100; /* 子要素(z-index: 999)よりも低い z-index */")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        background-color: #4CAF50;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        color: white;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        border: 3px solid #387638;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D77","--shiki-light":"#B5695977"}},'    "'),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},">")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"        別の兄弟要素 (z-index: 100)")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"    </"),s("span",{style:{"--shiki-dark":"#4D9375","--shiki-light":"#1E754F"}},"div"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},">")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"    <"),s("span",{style:{"--shiki-dark":"#4D9375","--shiki-light":"#1E754F"}},"div"),s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}}," style"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"="),s("span",{style:{"--shiki-dark":"#C98A7D77","--shiki-light":"#B5695977"}},'"')]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        /* スタッキングコンテキストを生成するプロパティ */")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        overflow: hidden; ")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        /* または transform: translateX(0); */")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        ")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        position: relative; /* 子要素の基準点とする */")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        width: 200px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        height: 150px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        margin: 50px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        padding: 10px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        border: 3px solid #007bff;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"        background-color: #e6f7ff;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D77","--shiki-light":"#B5695977"}},'    "'),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},">")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"        親要素 (overflow: hidden)")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"        ")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"        <"),s("span",{style:{"--shiki-dark":"#4D9375","--shiki-light":"#1E754F"}},"div"),s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}}," style"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"="),s("span",{style:{"--shiki-dark":"#C98A7D77","--shiki-light":"#B5695977"}},'"')]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            position: absolute;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            /* 親要素の右側、下側に大きくはみ出すように配置 */")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            top: 50px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            left: 150px; ")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            width: 150px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            height: 100px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            ")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            /* 別の兄弟要素よりもはるかに高い z-index を設定 */")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            z-index: 999; ")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            ")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            background-color: #ff4d4f;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            color: white;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            padding: 10px;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            border: 1px solid darkred;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D77","--shiki-light":"#B5695977"}},'        "'),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},">")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"            子要素 (z-index: 999)")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"        </"),s("span",{style:{"--shiki-dark":"#4D9375","--shiki-light":"#1E754F"}},"div"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},">")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"    </"),s("span",{style:{"--shiki-dark":"#4D9375","--shiki-light":"#1E754F"}},"div"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},">")])])],-1)])]),_:1},16)]),_:1},16)}}};export{z as default};
